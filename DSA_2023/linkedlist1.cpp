016f062fd715c617aae981d8093268506e026c1e
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2023-07-06 07:21:51 +0530

    Added changes to linkedlist

--- DSA_2023/linkedlist1.cpp
+++ DSA_2023/linkedlist1.cpp
@@ -1,13 +1,47 @@
 #include<iostream>
 using namespace std;
 
+struct Node{
+    int data;
+    Node *ptr;
+    Node(){
+        data = 0;
+        ptr = nullptr;
+    }
+    Node(int num){
+        this->data = num;
+        ptr = nullptr;
+    }
+};
 
 
+void disp(Node *head){
+    Node *temp = head;
+    while(temp!=nullptr){
+        cout << temp->data << " ";
+        temp = temp->ptr;
+    }
+    cout << endl;
+}
+
+void insert(Node **phead, int data, int pos){
+    /*
+    pos = 0 : insert at begin
+    pos = -1 : insert at end
+    */
+    if(pos == 0){
+        Node *temp = new Node(data);
+        temp->ptr = *phead;
+        //phead = &temp;
+        *phead = temp;       
+    }
+}
 
 int main(){
+    Node *head = new Node();
+    disp(head);
+    insert(&head, 5, 0);
+    disp(head);
 
-
-
-
-    
+    return 0;    
 }
