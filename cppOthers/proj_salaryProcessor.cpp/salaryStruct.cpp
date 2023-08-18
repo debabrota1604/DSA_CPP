728800b26676a83d40bea133c2945bc9c59d48ab
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2023-08-18 06:58:47 +0530

    Reorganisational Changes #2

---
+++ cppOthers/proj_salaryProcessor.cpp/salaryStruct.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class salaryComponents{
+    int basicPay, specialAllowence, houseRentAllowence, leaveTravelAllowence;
+    int foodCoupon, educationAllowence, broadbandAllowence;
+    int bonusMonthly, bonusQuarterly, bonusHalfYearly, bonusYearly;
+};
+
+class taxComponents{
+    int houseRentAllowence, invest_elss, invest_lifeInsurance, invest_providend_fund, invest_nps;
+    int nps_employerContributions, pf_employerContributions, gratuity;
+    int medicalIns_self, medicalIns_parent, healthcheckup_self, healthCheckup_parent;
+    int income_rental, income_savings_fd, income_stocks, income_mutualfund; 
+};
